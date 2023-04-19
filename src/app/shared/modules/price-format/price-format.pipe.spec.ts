1ce9fae827c2299de3ffee820eb1d138caf7c0f5
Nabendu Manna <69715068+Nabendu-Manna@users.noreply.github.com>
2023-04-20 01:37:45 +0530

    de ex

--- src/app/shared/modules/price-format/price-format.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PriceFormatPipe } from './price-format.pipe';
-
-describe('PriceFormatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PriceFormatPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
