91cdd44b9d8b169e18ef6b2a62ef794c82c6415a
Nabendu Manna <69715068+Nabendu-Manna@users.noreply.github.com>
2023-04-20 02:03:48 +0530

    d

--- src/app/shared/dialogs/donate-dialog/donate-dialog.module.ts
+++ src/app/shared/dialogs/donate-dialog/donate-dialog.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonMaterialModule } from "../../modules/common-material/common-material.module";
-import { DonateDialogComponent } from "./donate-dialogs.component";
+import { DonateDialogComponent } from "./donate-dialog.component";
 
 @NgModule({
     declarations: [
